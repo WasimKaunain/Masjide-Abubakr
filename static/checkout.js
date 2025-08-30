6e22278d5f2c5478be4231f0576acc5d38945bfd
WasimKaunain <ansariw580@gmail.com>
2025-08-30 09:56:09 +0530

    removed bugs

--- static/checkout.js
+++ static/checkout.js
@@ -9,17 +9,12 @@ fetch("/config")
 
 function goToPayment() {
   if (selectedAmount > 0) {
-    const upiId = "ansariw580@oksbi"; 
     const name = encodeURIComponent("Mosque Donation");
     const url = `upi://pay?pa=${upiId}&pn=${name}&am=${selectedAmount}&cu=INR`;
 
     // Open in same tab (works on mobile)
     window.location.href = url;
 
-    // (Optional fallback for desktop)
-    setTimeout(() => {
-      alert("UPI apps are not supported on desktop. Please try from your mobile device.");
-    }, 2000);
   } else {
     alert("Please select a donation amount first!");
   }
